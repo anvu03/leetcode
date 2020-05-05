9e58ebc2ccbdc4ed0c61e22d275f77c8799e7b04
An Vu <aqv1321@jagmail.southalabama.edu>
2020-05-04 23:21:59 -0500

    Add maximum-binary-tree-width

---
+++ maximum-binary-tree-width/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace maximum_binary_tree_width {
+    class Node {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+
+    class Program {
+        static int Min = int.MaxValue;
+        static int Max = int.MinValue;
+        static void Main (string[] args) {
+            var tree = CreateTree ();
+            Traverse (tree, 0);
+
+            Console.WriteLine ($"Max width = {Max - Min}");
+        }
+
+        static void Traverse (Node tree, int pos) {
+            if (pos < Min)
+                Min = pos;
+            if (pos > Max)
+                Max = pos;
+            if (tree.Left != null)
+                Traverse (tree.Left, pos - 1);
+            if (tree.Right != null)
+                Traverse (tree.Right, pos + 1);
+        }
+
+        static Node CreateTree () {
+            return new Node {
+                Left = new Node (),
+                    Right = new Node ()
+            };
+        }
+    }
+}
---
+++ maximum-binary-tree-width/README.md
@@ -0,0 +1,1 @@
+Find binary maximum width
